1ab318f7ae133b9c4552c249f777f33aa26186ab
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-07-21 13:08:32 +0300

    Tasks are saved in local storages and can by loaded already. Still need the buttons to delete from local storages

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
-
   const addButton = document.getElementById('add-task-btn');
   const taskInput = document.getElementById('task-input');
   const taskList = document.getElementById('task-list');
+
+  savedTasks = JSON.parse(localStorage.getItem('tasks'));
+  // console.log(`Ito ary: ${JSON.parse(savedTasks)}`);
+  // console.log(localStorage.tasks);
+  if (savedTasks){
+    taskList.innerHTML = savedTasks;
+  }
 
   function addTask(){
     const taskText = taskInput.value.trim();
@@ -23,15 +29,26 @@ document.addEventListener("DOMContentLoaded", function(){
         task.remove();
       }
       )
+
+      localStorage.setItem("tasks", JSON.stringify(taskList.innerHTML));
     }
   }
+  
+  const deleteButton = document.querySelectorAll(".remove-btn");
+  deleteButton.forEach((elt) => {
+    elt.addEventListener("click", function(event){
+    event.target.parentElement.remove();
+  })});
+
 
   addButton.addEventListener("click", addTask);
   taskInput.addEventListener("keypress", function(event){
     if(event.key === "Enter"){
       addTask();
     }
-  })
+  });
+
+  
 
 }
 )
